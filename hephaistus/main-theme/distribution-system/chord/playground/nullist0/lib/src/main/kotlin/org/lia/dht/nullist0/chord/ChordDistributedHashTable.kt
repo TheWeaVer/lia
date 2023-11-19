925e812d31770af06238ab258fd978b0d4de8294
nullist0 <nullist.null@gmail.com>
2023-11-20 00:22:55 +0900

    CHORD-interface: Add empty classes for each interfaces of CHORD

---
+++ hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordDistributedHashTable.kt
@@ -0,0 +1,16 @@
+package org.lia.dht.nullist0.chord
+
+import org.lia.dht.nullist0.DistributedHashTable
+
+/**
+ * An implementation of [DistributedHashTable] for CHORD algorithm.
+ */
+internal class ChordDistributedHashTable<K, V>: DistributedHashTable<K, V> {
+    override fun get(key: K): V {
+        TODO("Not yet implemented")
+    }
+
+    override fun set(key: K, value: V) {
+        TODO("Not yet implemented")
+    }
+}
