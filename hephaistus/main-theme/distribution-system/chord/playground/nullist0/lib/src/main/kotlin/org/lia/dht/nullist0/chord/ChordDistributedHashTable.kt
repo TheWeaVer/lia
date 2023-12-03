0a6e74a9d97d84ea9c9924b8dbd4f8bc0669f9e4
nullist0 <nullist.null@gmail.com>
2023-12-03 23:57:54 +0900

    CHORD-repository: Implement stabilize, notify, checkPredecessor
    CHORD-repository: Implement stabilize, notify, checkPredecessor, fixFingers

--- hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordDistributedHashTable.kt
+++ hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordDistributedHashTable.kt
@@ -1,16 +1,63 @@
 package org.lia.dht.nullist0.chord
 
 import org.lia.dht.nullist0.DistributedHashTable
+import org.lia.dht.nullist0.chord.model.ChordNode
 
 /**
  * An implementation of [DistributedHashTable] for CHORD algorithm.
  */
-internal class ChordDistributedHashTable<K, V>: DistributedHashTable<K, V> {
+internal class ChordDistributedHashTable<K, V>(
+    private val chordNode: ChordNode,
+    private val repository: ChordRepository<Int, V>
+): DistributedHashTable<K, V> {
+    private var finger: List<ChordNode> = emptyList()
+    private var successor: ChordNode = chordNode
+    private var predecessorOrNull: ChordNode? = null
+
     override fun get(key: K): V {
-        TODO("Not yet implemented")
+        val node = repository.findSuccessor(chordNode, successor, finger, key.toId())
+        return repository.getValue(key.toId(), node)
     }
 
     override fun set(key: K, value: V) {
-        TODO("Not yet implemented")
+        val node = repository.findSuccessor(chordNode, successor, finger, key.toId())
+        repository.putValue(key.toId(), value, node)
     }
+
+    private fun stabilize() {
+        val predecessorOfSuccessor = repository.findPredecessor(successor)
+        val ringRange = openInterval(chordNode.toId(), successor.toId())
+        if (predecessorOfSuccessor.toId() in ringRange) {
+            successor = predecessorOfSuccessor
+        }
+        repository.notify(successor, chordNode)
+    }
+
+    private fun fixFingers() {
+        finger = repository.findFingers(chordNode)
+    }
+
+    private fun notify(node: ChordNode) {
+        if (predecessorOrNull == null) {
+            predecessorOrNull = node
+            return
+        }
+        val predecessor = predecessorOrNull ?: return
+        val ringRange = openInterval(predecessor.toId(), chordNode.toId())
+        if (node.toId() in ringRange) {
+            predecessorOrNull = node
+        }
+    }
+
+    private fun checkPredecessor() {
+        predecessorOrNull = predecessorOrNull?.takeIf { !repository.healthCheck(it) }
+    }
+
+    // TODO: Change to Hash algorithm
+    private fun K.toId(): Int = hashCode()
+
+    // TODO: Change to Hash algorithm
+    private fun ChordNode.toId(): Int = hashCode()
+
+    private fun openInterval(n: Int, m: Int): IntRange = n + 1 until m
 }
