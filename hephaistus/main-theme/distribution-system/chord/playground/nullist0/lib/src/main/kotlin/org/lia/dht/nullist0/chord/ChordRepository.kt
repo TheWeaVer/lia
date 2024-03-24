527b516759ccfede8e33bbdfc100bfbc89fc6c20
nullist0 <nullist.null@gmail.com>
2024-03-24 13:41:05 +0900

    CHORD-node: Create ChordRepository for data of a node

---
+++ hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordRepository.kt
@@ -0,0 +1,11 @@
+package org.lia.dht.nullist0.chord
+
+import org.lia.dht.nullist0.chord.model.ChordNode
+
+/**
+ * An interface to abstract the local data source for the chord ring.
+ */
+interface ChordRepository<Id, Value> {
+    fun addData(dataNode: ChordNode.DataNode<Id, Value>)
+    fun getData(id: Id): ChordNode.DataNode<Id, Value>
+}
