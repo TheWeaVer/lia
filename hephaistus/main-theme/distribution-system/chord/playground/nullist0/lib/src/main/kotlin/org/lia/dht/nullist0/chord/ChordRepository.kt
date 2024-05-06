714400da524d4c5901e87ec1aad7f91a3c3d3b1f
nullist0 <nullist.null@gmail.com>
2024-05-06 20:05:40 +0900

    Address comment: Fix comment of interfaces

--- hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordRepository.kt
+++ hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/ChordRepository.kt
@@ -3,7 +3,7 @@ package org.lia.dht.nullist0.chord
 import org.lia.dht.nullist0.chord.model.ChordNode
 
 /**
- * An interface to abstract the local data source for the chord ring.
+ * An interface of storage for the chord ring.
  */
 interface ChordRepository<Id, Value> {
     fun addData(dataNode: ChordNode.DataNode<Id, Value>)
--- hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/event/ChordEvent.kt
+++ hephaistus/main-theme/distribution-system/chord/playground/nullist0/lib/src/main/kotlin/org/lia/dht/nullist0/chord/event/ChordEvent.kt
@@ -3,11 +3,11 @@ package org.lia.dht.nullist0.chord.event
 import org.lia.dht.nullist0.chord.ChordConfiguration
 
 /**
- * An interface to represent the domain event of chord algorithm.
+ * An interface to represent the operators of chord algorithm.
  */
 internal interface ChordEvent<Id, Value, ReturnType>: Function<ReturnType> {
     /**
-     * A function to implement a domain logic for each event.
+     * A function to implement a specific logic for each event.
      */
     suspend operator fun invoke(configuration: ChordConfiguration<Id, Value>): ReturnType
 }
